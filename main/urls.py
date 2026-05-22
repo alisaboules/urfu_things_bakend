cbde45d971da6a55ac84dedd60e410f67a308851
Алиса <alisia.bajeeva247@gmail.com>
2026-05-22 17:17:49 +0500

    hhhh

--- main/urls.py
+++ main/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     # Логи
     path('logs/', views.LogListView.as_view(), name='log-list'),
     path('logs/<int:pk>/', views.LogDetailView.as_view(), name='log-detail'),
-    
+    path('me/', views.MeView.as_view(), name='me'),
     # Геолокация и пункты выдачи
     path('pickup-points/nearby/', views.NearestPickupPointView.as_view(), name='nearest-pickup-point'),
     path('pickup-points/suggest/', views.AutoSuggestPickupPointView.as_view(), name='suggest-pickup-point'),
