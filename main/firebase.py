ddb28934991c41eb4abf5998bae9eb85644ccbee
Алиса <alisia.bajeeva247@gmail.com>
2026-05-23 13:08:38 +0500

    fix env without secrets

--- main/firebase.py
+++ main/firebase.py
@@ -1,15 +1,24 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials
-import firebase_admin.messaging as messaging
+from firebase_admin import credentials, messaging
+from dotenv import load_dotenv
+load_dotenv()
+
 firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
 
-cred_dict = json.loads(firebase_json)
+if not firebase_json:
+    raise Exception("FIREBASE_CREDENTIALS is missing")
 
-cred = credentials.Certificate(cred_dict)
+try:
+    cred_dict = json.loads(firebase_json)
+except json.JSONDecodeError as e:
+    raise Exception(f"Invalid FIREBASE_CREDENTIALS JSON: {e}")
 
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+
 
 def send_push(token, title, body):
     message = messaging.Message(
