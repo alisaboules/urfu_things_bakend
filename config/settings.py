d4881103d301912a368b5ecc41e78b23795eac83
Алиса <alisia.bajeeva247@gmail.com>
2026-05-24 01:58:09 +0500

    jj

--- config/settings.py
+++ config/settings.py
@@ -148,4 +148,11 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get("CLOUDINARY_SECRET"),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
