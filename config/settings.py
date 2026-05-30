737ce76f795bc91d154860dae3e3358918bed390
Алиса <alisia.bajeeva247@gmail.com>
2026-05-30 12:13:59 +0500

    jj

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # 10 элементов на странице
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 # JWT настройки
